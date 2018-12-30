258ff20aac4f9336de54424a59a32702adf1f36c
iceza1611 <siradanaiboonnak@hotmail.com>
2018-12-30 20:27:33 +0700

    no message

---
+++ src/main/java/com/multipleprofiles/MultipleprofilesApplication.java
@@ -0,0 +1,14 @@
+package com.multipleprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleprofilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleprofilesApplication.class, args);
+	}
+
+}
+
